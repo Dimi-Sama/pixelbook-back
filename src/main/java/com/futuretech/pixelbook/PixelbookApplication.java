1fb32cc77ebe6bc8c98c62160530107dda2f88e4
Dimi-Sama <dimitry97490@gmail.com>
2025-04-10 22:46:46 +0400

    init commit

---
+++ src/main/java/com/futuretech/pixelbook/PixelbookApplication.java
@@ -0,0 +1,13 @@
+package com.futuretech.pixelbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelbookApplication.class, args);
+	}
+
+}
